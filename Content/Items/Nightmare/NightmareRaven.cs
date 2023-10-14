385e5176d0dd58f8a5eb61b9f6757245c121c6e3
CoraIite <793708493@qq.com>
2023-10-14 21:57:50 +0800

    捕梦网

---
+++ Content/Items/Nightmare/NightmareRaven.cs
@@ -0,0 +1,59 @@
+﻿using Coralite.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Coralite.Content.Items.Nightmare
+{
+    public class NightmareRaven:ModProjectile
+    {
+        public override string Texture => AssetDirectory.NightmarePlantera+ "NightmareCrow";
+
+        public Color drawColor;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+            ProjectileID.Sets.TrailCacheLength[Type] = 10;
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+        }
+
+        public override void AI()
+        {
+            base.AI();
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D mainTex = TextureAssets.Projectile[Projectile.type].Value;
+            Vector2 pos = Projectile.Center - Main.screenPosition;
+            Rectangle frameBox = mainTex.Frame(1, 5, 0, Projectile.frame);
+            Vector2 origin = frameBox.Size() / 2;
+            SpriteEffects effect = Projectile.direction > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            //绘制残影
+            Vector2 toCenter = new Vector2(Projectile.width / 2, Projectile.height / 2);
+
+            for (int i = 0; i < 10; i += 1)
+                Main.spriteBatch.Draw(mainTex, Projectile.oldPos[i] + toCenter - Main.screenPosition, frameBox,
+                    drawColor * (0.5f - i * 0.05f), Projectile.oldRot[i], frameBox.Size() / 2, 1, effect, 0);
+
+            //向上下左右四个方向绘制一遍
+            for (int i = 0; i < 4; i++)
+            {
+                Main.spriteBatch.Draw(mainTex, pos + (i * MathHelper.PiOver2).ToRotationVector2() * 4, frameBox, drawColor, Projectile.rotation, origin, 1,
+                   effect, 0);
+            }
+
+            //绘制自己
+            Main.spriteBatch.Draw(mainTex, pos, frameBox, Color.White, Projectile.rotation, origin, 1, effect, 0);
+            return false;
+        }
+    }
+}
---
+++ Content/Items/Nightmare/Weapon.Dreamcatcher.cs
@@ -0,0 +1,68 @@
+﻿using Coralite.Core;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Coralite.Content.Items.Nightmare
+{
+    public class Dreamcatcher:ModItem,INightmareWeapon
+    {
+        public override string Texture => AssetDirectory.NightmareItems + Name;
+
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 40;
+            Item.damage = 6;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.reuseDelay = 25;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.holdStyle = ItemHoldStyleID.HoldLamp;
+            Item.DamageType = DamageClass.Summon;
+            Item.mana = 20;
+            Item.knockBack = 3;
+
+            Item.value = Item.sellPrice(0, 0, 1, 0);
+            Item.rare = RarityType<NightmareRarity>();
+            Item.shoot = ProjectileType<NightmareRaven>();
+            Item.shootSpeed = 0.1f;
+
+            Item.useTurn = false;
+            Item.noMelee = true;
+            Item.noUseGraphic = false;
+            Item.autoReuse = false;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            Lighting.AddLight(player.Center + player.velocity + new Vector2(player.direction * 24, 16), TorchID.Red);
+        }
+
+        public override void HoldStyle(Player player, Rectangle heldItemFrame)
+        {
+            float rot = player.velocity.X / 15;
+            if (MathF.Abs(rot) > 0.6f)
+                rot = Math.Sign(rot) * 0.6f;
+
+            player.itemRotation = rot;
+            Vector2 offset = ((player.itemRotation - MathHelper.PiOver2).ToRotationVector2() * 68);
+            player.itemLocation += new Vector2(player.direction * -6 - offset.X, 44 - (68 + offset.Y) * 2);
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            position = player.Center + player.velocity + new Vector2(player.direction * 24, 16);
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+            projectile.originalDamage = Item.damage;
+            return false;
+        }
+    }
+}
