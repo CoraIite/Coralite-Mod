7c6a1c1dacde05c4c4aeb66c798c9df352d7a119
CoraIite <793708493@qq.com>
2025-01-15 22:25:51 +0800

    震波制作中

--- Content/Items/ThyphionSeries/SeismicWave.cs
+++ Content/Items/ThyphionSeries/SeismicWave.cs
@@ -1,9 +1,130 @@
-﻿using Coralite.Core;
+﻿using Coralite.Content.Items.Steel;
+using Coralite.Content.ModPlayers;
+using Coralite.Core;
+using Coralite.Core.Prefabs.Projectiles;
+using Coralite.Core.Systems.CameraSystem;
+using Coralite.Helpers;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
 
 namespace Coralite.Content.Items.ThyphionSeries
 {
-    public class SeismicWave:ModItem
+    public class SeismicWave : ModItem, IDashable
     {
         public override string Texture => AssetDirectory.ThyphionSeriesItems + Name;
+
+        public float Priority => IDashable.HeldItemDash;
+
+        public override void SetDefaults()
+        {
+            Item.SetWeaponValues(55, 6f);
+            Item.DefaultToRangedWeapon(10, AmmoID.Arrow, 27, 13f);
+
+            Item.rare = ItemRarityID.Pink;
+            Item.useStyle = ItemUseStyleID.Rapier;
+            Item.value = Item.sellPrice(0, 4);
+
+            Item.noUseGraphic = true;
+            Item.useTurn = false;
+            Item.UseSound = CoraliteSoundID.Bow_Item5;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if (player.TryGetModPlayer(out CoralitePlayer cp))
+                cp.AddDash(this);
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 dir = (Main.MouseWorld - player.Center).SafeNormalize(Vector2.One);
+            float rot = dir.ToRotation();
+
+            Projectile.NewProjectile(new EntitySource_ItemUse(player, Item)
+                , player.Center, Vector2.Zero, ProjectileType<FullMoonHeldProj>(), damage, knockback, player.whoAmI, rot);
+
+            Projectile.NewProjectile(source, player.Center, velocity, type, damage, knockback, player.whoAmI);
+
+            return false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<SteelBar>(15)
+                .AddIngredient(ItemID.HallowedBar,12)
+                .AddIngredient(ItemID.SoulofMight,12)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+
+        public bool Dash(Player Player, int DashDir)
+        {
+            Vector2 newVelocity = Player.velocity;
+            float dashDirection;
+            switch (DashDir)
+            {
+                case CoralitePlayer.DashDown:
+                    dashDirection = DashDir == CoralitePlayer.DashRight ? 1 : -1;
+                    break;
+                default:
+                    return false;
+            }
+
+            Player.GetModPlayer<CoralitePlayer>().DashDelay = 90;
+            Player.GetModPlayer<CoralitePlayer>().DashTimer = 22;
+            Player.velocity = newVelocity;
+            //Player.direction = (int)dashDirection;
+            Player.AddImmuneTime(ImmunityCooldownID.General, 14);
+            Player.immune = true;
+
+            Main.instance.CameraModifiers.Add(new MoveModifyer(5, 15));
+
+            if (Player.whoAmI == Main.myPlayer)
+            {
+                //Helper.PlayPitched("Misc/HallowDash", 0.4f, -0.2f, Player.Center);
+                Helper.PlayPitched(CoraliteSoundID.Swing_Item1, Player.Center);
+
+                foreach (var proj in from proj in Main.projectile
+                                     where proj.active && proj.friendly && proj.owner == Player.whoAmI && proj.type == ProjectileType<SeismicWaveHeldProj>()
+                                     select proj)
+                {
+                    proj.Kill();
+                    break;
+                }
+
+                //生成手持弹幕
+                Projectile.NewProjectile(Player.GetSource_ItemUse(Player.HeldItem), Player.Center, new Vector2(dashDirection, 0), ProjectileType<SeismicWaveHeldProj>(),
+                    Player.HeldItem.damage, Player.HeldItem.knockBack, Player.whoAmI, newVelocity.ToRotation(), 1, 18);
+            }
+
+            return true;
+        }
+    }
+
+    public class SeismicWaveHeldProj:BaseDashBow
+    {
+        public override string Texture => AssetDirectory.ThyphionSeriesItems + nameof(SeismicWave);
+
+        public ref float DashState => ref Projectile.localAI[0];
+        public ref float Timer => ref Projectile.localAI[1];
+        public ref float RecordAngle => ref Projectile.localAI[2];
+
+        public override int GetItemType()
+            => ItemType<SeismicWave>();
+
+        public override void DashAttackAI()
+        {
+            base.DashAttackAI();
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            return base.PreDraw(ref lightColor);
+        }
     }
 }
