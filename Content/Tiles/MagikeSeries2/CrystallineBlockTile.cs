75bef2a120d2699625a571c734c07558e0599711
CoraIite <793708493@qq.com>
2025-02-24 21:10:00 +0800

    新增蕴魔水晶块

---
+++ Content/Tiles/MagikeSeries2/CrystallineBlockTile.cs
@@ -0,0 +1,27 @@
+﻿using Coralite.Core;
+using Terraria;
+using Terraria.ID;
+
+namespace Coralite.Content.Tiles.MagikeSeries2
+{
+    public class CrystallineBlockTile : ModTile
+    {
+        public override string Texture => AssetDirectory.MagikeSeries2Tile + Name;
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            TileID.Sets.CanBeClearedDuringGeneration[Type] = false;
+            TileID.Sets.CanBeClearedDuringOreRunner[Type] = false;
+
+            MineResist = 2f;
+            DustType = DustID.PurpleTorch;
+            HitSound = SoundID.DD2_CrystalCartImpact;
+            MinPick = 110;
+
+            AddMapEntry(Coralite.CrystallineMagikePurple);
+        }
+    }
+}
