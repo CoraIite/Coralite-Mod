6b4aab16355f15979509f4259d0af62176376a55
CoraIite <793708493@qq.com>
2025-06-03 20:33:30 +0800

    BUG修复

--- Core/Systems/MagikeSystem/TileEntities/Pedestal.cs
+++ Core/Systems/MagikeSystem/TileEntities/Pedestal.cs
@@ -5,6 +5,8 @@ namespace Coralite.Core.Systems.MagikeSystem.TileEntities
     public abstract class Pedestal<TModTile>() : MagikeTP()
         where TModTile : ModTile
     {
+        public override int ExtendFilterCapacity => 1;
+
         public sealed override int TargetTileID => (ushort)ModContent.TileType<TModTile>();
 
         public override int MainComponentID => MagikeComponentID.ItemContainer;
