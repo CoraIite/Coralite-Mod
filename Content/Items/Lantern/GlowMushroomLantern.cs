4162cdf1bc22cdebf417df7533ef6668051cf763
CoraIite <793708493@qq.com>
2025-06-14 15:21:33 +0800

    夜光蘑菇提灯优化

--- Content/Items/Lantern/GlowMushroomLantern.cs
+++ Content/Items/Lantern/GlowMushroomLantern.cs
@@ -21,9 +21,8 @@ namespace Coralite.Content.Items.Lantern
             Item.SetWeaponValues(15, 3.5f);
             Item.SetShopValues(Terraria.Enums.ItemRarityColor.Blue1, Item.sellPrice(silver: 50));
 
-
             Item.mana = 12;
-            Item.useTime = Item.useAnimation = 25;
+            Item.useTime = Item.useAnimation = 30;
             Item.holdStyle = ItemHoldStyleID.HoldLamp;
             Item.useStyle = ItemUseStyleID.RaiseLamp;
             Item.UseSound = CoraliteSoundID.FireHitDD2_BetsysWrathImpact;
@@ -31,21 +30,27 @@ namespace Coralite.Content.Items.Lantern
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Vector2 pos = player.Center + new Vector2(player.direction * 10, -4);
+            Vector2 pos = player.Center + new Vector2(player.direction * 22, -4);
 
             Projectile.NewProjectile(source, pos, velocity, type, damage, knockback, player.whoAmI);
 
+            for (int i = 0; i < 5; i++)
+            {
+                Color c = Main.rand.NextFromList(new Color(25, 142, 196), new Color(20, 78, 129)) * 0.5f;
+                PRTLoader.NewParticle<Fog>(pos + Main.rand.NextVector2Circular(12, 12)
+                    , Helper.NextVec2Dir(0.25f, 0.6f), c, Main.rand.NextFloat(0.3f, 0.7f));
+
+                Dust d = Dust.NewDustPerfect(pos + Main.rand.NextVector2Circular(4, 4)
+                    , DustID.MushroomTorch, Helper.NextVec2Dir(1, 2.5f), Scale: Main.rand.NextFloat(1, 1.5f));
+
+                d.noGravity = true;
+
+                d = Dust.NewDustPerfect(pos + Main.rand.NextVector2Circular(4, 4)
+                    , DustID.GlowingMushroom, Helper.NextVec2Dir(1, 1.5f), Scale: Main.rand.NextFloat(1, 1.5f));
+
+                d.noGravity = true;
+            }
             return false;
-        }
-
-        public override Vector2? HoldoutOffset()
-        {
-            return new Vector2(0, 12);
-        }
-
-        public override Vector2? HoldoutOrigin()
-        {
-            return base.HoldoutOrigin();
         }
 
         public override void HoldStyle(Player player, Rectangle heldItemFrame)
@@ -56,6 +61,23 @@ namespace Coralite.Content.Items.Lantern
         public override void HoldItem(Player player)
         {
             Lighting.AddLight(player.Center + new Vector2(player.direction * 8, 4), TorchID.Mushroom);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.WandofSparking)
+                .AddIngredient(ItemID.MushroomTorch,15)
+                .AddIngredient(ItemID.SilverBar,5)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+
+            CreateRecipe()
+                .AddIngredient(ItemID.WandofSparking)
+                .AddIngredient(ItemID.MushroomTorch,15)
+                .AddIngredient(ItemID.TungstenBar,5)
+                .AddTile(TileID.WorkBenches)
+                .Register();
         }
     }
 
@@ -88,14 +110,14 @@ namespace Coralite.Content.Items.Lantern
                 Projectile.velocity.Y += 0.07f;
 
             if (Main.rand.NextBool())
-                Projectile.SpawnTrailDust(ModContent.DustType<PixelPoint>(), Main.rand.NextFloat(0, 0.4f), newColor: new Color(23, 231, 155)
+                Projectile.SpawnTrailDust(ModContent.DustType<PixelPoint>(), Main.rand.NextFloat(0, 0.4f), newColor: Color.DarkBlue
                     , Scale: Main.rand.NextFloat(0.75f, 1.25f));
 
             for (int i = 0; i < 2; i++)
-                Projectile.SpawnTrailDust(8f, DustID.MushroomTorch, Main.rand.NextFloat(-0.1f, 0.4f)
+                Projectile.SpawnTrailDust(4f, DustID.MushroomTorch, Main.rand.NextFloat(-0.1f, 0.4f)
                     , Scale: Main.rand.NextFloat(1f, 1.75f));
 
-            if (Main.rand.NextBool(3))
+            if (Main.rand.NextBool(4))
             {
                 Dust.NewDustPerfect(Projectile.Center, DustID.MushroomTorch
                     , -Vector2.UnitY.RotateByRandom(-0.5f, 0.5f) * Main.rand.NextFloat(1, 3), Scale: Main.rand.NextFloat(0.75f, 1.25f));
@@ -117,14 +139,25 @@ namespace Coralite.Content.Items.Lantern
                 for (int i = -1; i < 2; i++)
                 {
                     Projectile.NewProjectileFromThis<GlowMushroomLanternGrow>(Projectile.Center
-                        , -Vector2.UnitY.RotateByRandom(i * 0.7f - 0.35f, i + 0.7f + 0.35f) * Main.rand.NextFloat(3,7), damage, Projectile.knockBack);
+                        , -Vector2.UnitY.RotateByRandom(i * 1f - 0.75f, i + 1f + 0.75f) * Main.rand.NextFloat(2,6.5f), damage, Projectile.knockBack);
                 }
             }
 
             //生成粒子
             if (!VaultUtils.isServer)
             {
+                for (int i = 0; i < 8; i++)
+                {
+                    Dust d = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(4, 4)
+                        , DustID.MushroomTorch, Helper.NextVec2Dir(2, 3.5f), Scale:Main.rand.NextFloat(1.5f,2f));
 
+                    d.noGravity = true;
+
+                    d = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(4, 4)
+                        , DustID.GlowingMushroom, Helper.NextVec2Dir(1, 1.5f));
+
+                    d.noGravity = true;
+                }
             }
         }
 
@@ -169,7 +202,7 @@ namespace Coralite.Content.Items.Lantern
             {
                 float angle = Projectile.velocity.ToRotation();
 
-                angle = angle.AngleLerp(-MathHelper.PiOver2, 0.07f);
+                angle = angle.AngleLerp(-MathHelper.PiOver2, 0.1f);
                 Projectile.velocity = angle.ToRotationVector2() * Projectile.velocity.Length();
 
                 if (Main.rand.NextBool())
@@ -189,7 +222,6 @@ namespace Coralite.Content.Items.Lantern
 
             if (!VaultUtils.isServer)
             {
-
                 Projectile.UpdateOldPosCache();
                 Projectile.UpdateOldRotCache();
             }
@@ -202,11 +234,20 @@ namespace Coralite.Content.Items.Lantern
 
             for (int i = 0; i < 5; i++)
             {
-                Color c = Main.rand.NextFromList(new Color(25,142,196), new Color(20, 78, 129));
+                Color c = Main.rand.NextFromList(new Color(25,142,196), new Color(20, 78, 129)) * 0.5f;
                 PRTLoader.NewParticle<Fog>(Projectile.Center + Main.rand.NextVector2Circular(12, 12)
-                    , Helper.NextVec2Dir(0.5f, 1f), c, Main.rand.NextFloat(0.2f, 0.5f));
-            }
+                    , Helper.NextVec2Dir(0.25f, 0.6f), c, Main.rand.NextFloat(0.3f, 0.7f));
 
+                Dust d = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(4, 4)
+                    , DustID.MushroomTorch, Helper.NextVec2Dir(2, 2.5f), Scale: Main.rand.NextFloat(1, 1.5f));
+
+                d.noGravity = true;
+
+                d = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(4, 4)
+                    , DustID.GlowingMushroom, Helper.NextVec2Dir(1, 1.5f), Scale: Main.rand.NextFloat(1, 1.5f));
+
+                d.noGravity = true;
+            }
         }
 
         public override bool PreDraw(ref Color lightColor)
@@ -222,10 +263,10 @@ namespace Coralite.Content.Items.Lantern
                     float factor = (float)i / 14;
                     Vector2 Center = Projectile.oldPos[i] - Main.screenPosition;
                     Vector2 normal = (Projectile.oldRot[i] + MathHelper.PiOver2).ToRotationVector2();
-                    Vector2 Top = Center + (normal * 1.5f);
-                    Vector2 Bottom = Center - (normal * 1.5f);
+                    Vector2 Top = Center + (normal * 2f);
+                    Vector2 Bottom = Center - (normal * 2f);
 
-                    var color = Color.Lerp(new Color(23, 231, 255), new Color(20, 78, 129), factor) * factor;
+                    var color = /*Color.Lerp(new Color(20, 78, 129), , factor)*/ new Color(23, 231, 255) * factor*0.75f;
                     bars.Add(new(Top, color, new Vector3(factor, 0, 1)));
                     bars.Add(new(Bottom, color, new Vector3(factor, 1, 1)));
                 }
@@ -234,7 +275,7 @@ namespace Coralite.Content.Items.Lantern
                 Main.graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, bars.ToArray(), 0, bars.Count - 2);
             }
 
-            Projectile.QuickDraw(lightColor, 0);
+            Projectile.QuickDraw(lightColor with { A=125}, 0);
 
             return false;
         }
