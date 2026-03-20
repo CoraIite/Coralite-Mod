ab0eab0350ca0a3165f1f97ef3eee149dfcf4750
CoraIite <793708493@qq.com>
2026-03-20 13:39:22 +0800

    常规同步

---
+++ Content/Items/MagikeSeries1/Wall.BasaltBrickWallItem.cs
@@ -0,0 +1,24 @@
+﻿using Coralite.Content.Walls.Magike;
+using Coralite.Core;
+using Terraria.ID;
+
+namespace Coralite.Content.Items.MagikeSeries1
+{
+    public class BasaltBrickWallItem : ModItem
+    {
+        public override string Texture => AssetDirectory.MagikeSeries1Item + Name;
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableWall(ModContent.WallType<BasaltBrickWall>());
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(4)
+                .AddIngredient<BasaltBrick>()
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
---
+++ Content/Walls/Magike/BasaltBrickWall.cs
@@ -0,0 +1,21 @@
+﻿using Coralite.Core;
+using Terraria;
+using Terraria.ID;
+
+namespace Coralite.Content.Walls.Magike
+{
+    public class BasaltBrickWall : ModWall
+    {
+        public override string Texture => AssetDirectory.MagikeWalls + Name;
+        public override void SetStaticDefaults()
+        {
+            Main.wallHouse[Type] = true;
+            DustType = DustID.CorruptionThorns;
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
