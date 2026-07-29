f13f07a6e3108be70ffbdcbae1f451dcab126593
CoraIite <793708493@qq.com>
2026-07-29 23:27:21 +0800

    常规同步

--- Content/Bosses/ShadowBalls/Others.Anmations.cs
+++ Content/Bosses/ShadowBalls/Others.Anmations.cs
@@ -1,4 +1,6 @@
-﻿namespace Coralite.Content.Bosses.ShadowBalls;
+﻿using System;
+
+namespace Coralite.Content.Bosses.ShadowBalls;
 
 public partial class ShadowBall
 {
@@ -8,5 +10,23 @@ public partial class ShadowBall
         MaskAlpha = 0f;
 
         Timer++;
+
+        //NPC.velocity.X = MathF.Sin(Timer * 0.02f)*10;
+        LockDistancePercent = 1 + MathF.Sin(Timer * 0.05f)*0.4f;
+        /*
+         * 从裂隙中探出发光核心
+         * 之后裂隙生成一堆影子弹幕
+         * 影子弹幕聚集到核心身上逐渐出现求成，之后开始吼叫？
+         * 吼叫时候生成环与名字
+         * 
+         * 之后切换到召唤小影子球阶段
+         */
+
+        switch (SonState)
+        {
+            default:
+            case 0://探出脑袋
+                break;
+        }
     }
 }
